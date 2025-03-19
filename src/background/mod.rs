07caee3bd93743258d1dfc4693f878997d46a944
myyrakle <sssang97@naver.com>
2025-03-20 00:31:56 +0900

    [#25] runner loop, scheduler loop 분리

--- src/background/mod.rs
+++ src/background/mod.rs
@@ -1,12 +1,11 @@
+pub mod runner;
+pub mod scheduler;
+
 use sea_orm::DatabaseConnection;
 
-pub fn start_background_loop(_database_connection: DatabaseConnection) -> anyhow::Result<()> {
-    tokio::spawn(async move {
-        loop {
-            tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
-            println!("Background loop");
-        }
-    });
+pub fn start_background_loop(database_connection: DatabaseConnection) -> anyhow::Result<()> {
+    runner::start_runner_loop(database_connection.clone())?;
+    scheduler::start_scheduler_loop(database_connection.clone())?;
 
     Ok(())
 }
