a5e7ef2bc0e16b4523ed80d6a35b6b7f313cc3c6
myyrakle <sssang97@naver.com>
2025-03-21 02:08:45 +0900

    [#21] 작업정의 생성시 command를 배열로 받게 변경

--- src/routes/task_definitions.rs
+++ src/routes/task_definitions.rs
@@ -50,13 +50,13 @@ pub async fn list_task_definitions(
 
 #[derive(Deserialize, Debug, Clone)]
 pub struct CreateTaskDefinitionBody {
-    pub name: String,              // task name
-    pub image: String,             // docker image
-    pub command: Option<String>,   // docker run command
-    pub args: Option<String>,      // docker run arguments
-    pub env: Option<String>,       // environment variables
-    pub memory_limit: Option<u32>, // memory limit in MB
-    pub cpu_limit: Option<u32>,    // cpu limit (default 1024)
+    pub name: String,                 // task name
+    pub image: String,                // docker image
+    pub command: Option<Vec<String>>, // docker run command
+    pub args: Option<String>,         // docker run arguments
+    pub env: Option<String>,          // environment variables
+    pub memory_limit: Option<u32>,    // memory limit in MB
+    pub cpu_limit: Option<u32>,       // cpu limit (default 1024)
 }
 
 pub async fn create_task_definition(
