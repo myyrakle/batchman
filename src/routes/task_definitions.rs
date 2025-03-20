2e65fe640afeb9639e1fb46d6f8e920f790da153
myyrakle <sssang97@naver.com>
2025-03-21 02:11:18 +0900

    [#21] 작업정의 조회시 command를 배열로 받게 변경

--- src/routes/task_definitions.rs
+++ src/routes/task_definitions.rs
@@ -8,7 +8,7 @@ use axum::{
 use sea_orm::DatabaseConnection;
 use serde::{Deserialize, Serialize};
 
-use crate::actions;
+use crate::{actions, db::entities};
 
 #[derive(Deserialize, Debug, Clone)]
 pub struct ListTaskDefinitionsQuery {
@@ -17,7 +17,38 @@ pub struct ListTaskDefinitionsQuery {
     pub name: Option<String>,
 }
 
-pub type ListTaskDefinitionsItem = crate::db::entities::task_definition::Model;
+#[derive(Serialize)]
+pub struct ListTaskDefinitionsItem {
+    pub id: i64,      // primary key
+    pub name: String, // task name
+    pub version: i64, // task version
+
+    pub image: String,                // docker image
+    pub command: Option<Vec<String>>, // docker run command
+    pub args: Option<String>,         // docker run arguments
+    pub env: Option<String>,          // environment variables
+
+    pub memory_limit: Option<u32>, // memory limit in MB
+    pub cpu_limit: Option<u32>,    // cpu limit (default 1024)
+}
+
+impl From<entities::task_definition::Model> for ListTaskDefinitionsItem {
+    fn from(model: entities::task_definition::Model) -> Self {
+        ListTaskDefinitionsItem {
+            id: model.id,
+            name: model.name,
+            version: model.version,
+            image: model.image,
+            command: model
+                .command
+                .map(|command| serde_json::from_str(&command).unwrap_or_default()),
+            args: model.args,
+            env: model.env,
+            memory_limit: model.memory_limit,
+            cpu_limit: model.cpu_limit,
+        }
+    }
+}
 
 #[derive(Serialize)]
 pub struct ListTaskDefinitionsResponse {
@@ -38,7 +69,12 @@ pub async fn list_task_definitions(
 
     match task_definitions {
         Ok(task_definitions) => {
-            let response = ListTaskDefinitionsResponse { task_definitions };
+            let response = ListTaskDefinitionsResponse {
+                task_definitions: task_definitions
+                    .into_iter()
+                    .map(ListTaskDefinitionsItem::from)
+                    .collect(),
+            };
             Json(response).into_response()
         }
         Err(error) => Response::builder()
