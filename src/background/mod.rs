face6f6677a5d5474612379d7ca2b1495a13fe10
myyrakle <sssang97@naver.com>
2025-03-22 03:24:26 +0900

    [#20] start_status_tracker_loop 틀 작성

--- src/background/mod.rs
+++ src/background/mod.rs
@@ -1,5 +1,6 @@
 pub mod runner;
 pub mod scheduler;
+pub mod status_tracker;
 
 use sea_orm::DatabaseConnection;
 
@@ -7,5 +8,6 @@ pub async fn start_background_loop(database_connection: DatabaseConnection) {
     tokio::join!(
         runner::start_runner_loop(database_connection.clone()),
         scheduler::start_scheduler_loop(database_connection.clone()),
+        status_tracker::start_status_tracker_loop(database_connection.clone()),
     );
 }
