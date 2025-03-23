6493a3d5f0bccad89cfbe0143a9ad4604977d7f8
myyrakle <sssang97@naver.com>
2025-03-24 01:42:42 +0900

    [#45] stop_job action 구현

---
+++ src/actions/stop_job.rs
@@ -0,0 +1,40 @@
+use sea_orm::{ColumnTrait, DatabaseConnection, EntityTrait, QueryFilter};
+
+use crate::{
+    db::entities::{self, job::JobStatus},
+    docker,
+};
+
+#[derive(Debug, Clone)]
+pub struct StopJobParams<'a> {
+    pub connection: &'a DatabaseConnection,
+    pub job_id: i64,
+}
+
+pub async fn stop_job(params: StopJobParams<'_>) -> anyhow::Result<()> {
+    let job_id = params.job_id;
+
+    let mut find_job_query = entities::job::Entity::find();
+
+    find_job_query = find_job_query.filter(entities::job::Column::Id.eq(job_id));
+
+    let Some(job) = find_job_query.one(params.connection).await? else {
+        return Err(anyhow::anyhow!("Job not found: {}", job_id));
+    };
+
+    if job.status == JobStatus::Finished {
+        return Err(anyhow::anyhow!("Job already finished"));
+    }
+
+    if job.status == JobStatus::Failed {
+        return Err(anyhow::anyhow!("Job already failed"));
+    }
+
+    let Some(container_id) = job.container_id.as_ref() else {
+        return Err(anyhow::anyhow!("Job has no container ID"));
+    };
+
+    docker::stop::stop_container(container_id, 3)?;
+
+    Ok(())
+}
