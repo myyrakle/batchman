c1969e7d72f554d9c5032832607a5bbda158c3cf
myyrakle <sssang97@naver.com>
2025-03-26 01:52:55 +0900

    [#49] schedule 모델에 job_name 필드 추가

--- src/db/entities/schedule.rs
+++ src/db/entities/schedule.rs
@@ -6,13 +6,11 @@ use sea_orm::entity::prelude::*;
 pub struct Model {
     #[sea_orm(primary_key)]
     pub id: i64, // primary key
-    pub name: String, // schedule name
-
-    pub cron_expression: String, // cron expression
-
-    pub task_definition_id: i64, // task definition id
-    pub command: Option<String>, // docker run command
-
+    pub name: String,                              // schedule name
+    pub job_name: String,                          // job name
+    pub cron_expression: String,                   // cron expression
+    pub task_definition_id: i64,                   // task definition id
+    pub command: Option<String>,                   // docker run command
     pub created_at: Option<chrono::DateTime<Utc>>, // job submited time
 }
 
