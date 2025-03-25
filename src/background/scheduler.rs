b2d739cd732697ba1e5662d5f0a3c01e45effb77
myyrakle <sssang97@naver.com>
2025-03-26 02:05:05 +0900

    [#51] schedule cdc event 타입 정의

--- src/background/scheduler.rs
+++ src/background/scheduler.rs
@@ -1,5 +1,34 @@
 use sea_orm::DatabaseConnection;
 
+use crate::db::entities;
+
+#[derive(Debug)]
+pub enum ScheduleCDCEvent {
+    New(NewSchedule),
+    Update(UpdateSchedule),
+    Delete(DeleteSchedule),
+}
+
+#[derive(Debug)]
+pub struct NewSchedule {
+    pub id: i64,
+    pub model: entities::schedule::Model,
+}
+
+#[derive(Debug)]
+pub struct UpdateSchedule {
+    pub id: i64,
+    pub model: entities::schedule::Model,
+}
+
+#[derive(Debug)]
+pub struct DeleteSchedule {
+    pub id: i64,
+}
+
+type ScheduleCDCSender = tokio::sync::mpsc::Sender<ScheduleCDCEvent>;
+type ScheduleCDCReceiver = tokio::sync::mpsc::Receiver<ScheduleCDCEvent>;
+
 pub async fn start_scheduler_loop(_database_connection: DatabaseConnection) {
     let _ = tokio::spawn(async move {
         loop {
