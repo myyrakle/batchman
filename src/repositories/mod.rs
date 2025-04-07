5641a99303c04fd4f50d1440f1ea143426961834
myyrakle <sssang97@naver.com>
2025-04-08 02:13:41 +0900

    [#61] list_task_definitions repository 구현

--- src/repositories/mod.rs
+++ src/repositories/mod.rs
@@ -1,8 +1,25 @@
+use sea_orm::prelude::async_trait::async_trait;
+
+use crate::db::entities;
+
 pub mod job;
 pub mod schedule;
 pub mod task_definition;
 
-pub trait TaskDefinitionRepository {}
+#[derive(Debug)]
+pub struct ListTaskDefinitionsParams {
+    pub name: Option<String>,
+    pub limit: Option<u64>,
+    pub order_by_desc: Option<entities::task_definition::Column>,
+}
+
+#[async_trait::async_trait]
+pub trait TaskDefinitionRepository {
+    async fn list_task_definitions(
+        &self,
+        params: ListTaskDefinitionsParams,
+    ) -> anyhow::Result<Vec<entities::task_definition::Model>>;
+}
 
 pub trait JobRepository {}
 
--- src/repositories/task_definition.rs
+++ src/repositories/task_definition.rs
@@ -1,10 +1,38 @@
-use super::TaskDefinitionRepository;
+use sea_orm::{ColumnTrait, EntityTrait, QueryFilter, QueryOrder, QuerySelect};
+
+use crate::{db::entities, routes::task_definitions};
+
+use super::{ListTaskDefinitionsParams, TaskDefinitionRepository};
 
 pub struct TaskDefinitionSeaOrmRepository {
     pub connection: sea_orm::DatabaseConnection,
 }
 
-impl TaskDefinitionRepository for TaskDefinitionSeaOrmRepository {}
+#[async_trait::async_trait]
+impl TaskDefinitionRepository for TaskDefinitionSeaOrmRepository {
+    async fn list_task_definitions(
+        &self,
+        params: ListTaskDefinitionsParams,
+    ) -> anyhow::Result<Vec<entities::task_definition::Model>> {
+        let mut find_query = entities::task_definition::Entity::find();
+
+        if let Some(name) = params.name {
+            find_query = find_query.filter(entities::task_definition::Column::Name.eq(name));
+        }
+
+        if let Some(order_by_desc) = params.order_by_desc {
+            find_query = find_query.order_by_desc(order_by_desc);
+        }
+
+        if let Some(limit) = params.limit {
+            find_query = find_query.limit(limit);
+        }
+
+        let task_definitions = find_query.all(&self.connection).await?;
+
+        Ok(task_definitions)
+    }
+}
 
 impl TaskDefinitionSeaOrmRepository {
     pub fn new(connection: sea_orm::DatabaseConnection) -> Self {
