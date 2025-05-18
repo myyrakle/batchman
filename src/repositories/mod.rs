fef8f86e48f25fa6d88f75fe15dbafb0f750d5a0
myyrakle <sssang97@naver.com>
2025-05-18 23:14:39 +0900

    [#63] list_jobs repository 인터페이스 정의

--- src/repositories/mod.rs
+++ src/repositories/mod.rs
@@ -94,10 +94,15 @@ pub struct PatchJobParams {
     pub error_message: Option<String>,              // batch error message
 }
 
+#[derive(Debug, Default)]
+pub struct ListJobsParams {
+    pub job_ids: Vec<i64>,
+}
+
 #[async_trait::async_trait]
 pub trait JobRepository {
+    async fn list_jobs(&self, params: ListJobsParams) -> anyhow::Result<Vec<entities::job::Model>>;
     async fn create_job(&self, params: CreateJobParams) -> anyhow::Result<i64>;
-
     async fn patch_job(&self, params: PatchJobParams) -> anyhow::Result<()>;
 }
 
