0732a93514c10f6f4668a3b279f9bee13214e176
myyrakle <sssang97@naver.com>
2025-05-26 00:39:45 +0900

    [#59] CronExpressionParseError 정의

--- src/types/cron.rs
+++ src/types/cron.rs
@@ -11,6 +11,8 @@ pub enum CronExpressionElement {
     Step(u32, u32),  // e.g., 0/15 (every 15 minutes)
 }
 
+// The CronExpression struct represents a cron expression.
+// example expression: "* * * * ? *"
 #[derive(Debug, Clone, PartialEq)]
 pub struct CronExpression {
     pub minutes: CronExpressionField,
@@ -18,5 +20,17 @@ pub struct CronExpression {
     pub day_of_month: CronExpressionField,
     pub month: CronExpressionField,
     pub day_of_week: CronExpressionField,
-    // pub year: Option<CronExpressionField>, // Year is optional spec
+    pub year: Option<CronExpressionField>,
 }
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct CronExpressionParseError {
+    pub message: String,
+}
+
+impl std::fmt::Display for CronExpressionParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Cron expression parse error: {}", self.message)
+    }
+}
+impl std::error::Error for CronExpressionParseError {}
