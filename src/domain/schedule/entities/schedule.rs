af0672773eb0ec2af559def1a83627454822696c
myyrakle <sssang97@naver.com>
2025-05-31 03:08:16 +0900

    [#50] last_triggered_at 필드 추가

--- src/domain/schedule/entities/schedule.rs
+++ src/domain/schedule/entities/schedule.rs
@@ -6,15 +6,16 @@ use sea_orm::entity::prelude::*;
 pub struct Model {
     #[sea_orm(primary_key)]
     pub id: i64, // primary key
-    pub name: String,                              // schedule name
-    pub job_name: String,                          // job name
-    pub cron_expression: String,                   // cron expression
-    pub task_definition_id: i64,                   // task definition id
-    pub command: Option<String>,                   // docker run command
-    pub timezone: Option<String>,                  // timezone text (example: "Asia/Seoul")
+    pub name: String,                                     // schedule name
+    pub job_name: String,                                 // job name
+    pub cron_expression: String,                          // cron expression
+    pub task_definition_id: i64,                          // task definition id
+    pub command: Option<String>,                          // docker run command
+    pub timezone: Option<String>,                         // timezone text (example: "Asia/Seoul")
     pub timezone_offset: Option<i32>, // timezone offset (in minutes) (example: 540=9:00 for "Asia/Seoul")
     pub enabled: bool,                // schedule enabled status
     pub created_at: Option<chrono::DateTime<Utc>>, // job submited time
+    pub last_triggered_at: Option<chrono::DateTime<Utc>>, // last triggered time
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
