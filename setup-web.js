44712148e174844f06c1f7a46a4c916396d1504f
myyrakle <sssang97@naver.com>
2025-06-03 02:11:30 +0900

    [#83] bundle=>rust 통합 스크립트 작성

---
+++ setup-web.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+async function main() {
+  const sourceBundlePath = "./web/dist/bundle.js";
+  const sourceBundleText = await fs.promises.readFile(sourceBundlePath, "utf8");
+
+  const targetBundlePath = "./src/jsbundle.rs";
+  const targetBundleCode = `pub const JS_BUNDLE: &'static str = r##"${sourceBundleText}"##;`;
+  await fs.promises.writeFile(targetBundlePath, targetBundleCode);
+}
+
+main();
